3003393438442ff567b3acf00ea7a3e3c2b9a914
mutatrum <mutatrum@gmail.com>
2024-09-01 15:21:11 +0200

    Fix nostr walletconnect URI

--- showWalletConnectQR.js
+++ showWalletConnectQR.js
@@ -7,7 +7,7 @@ const _relay = process.env.LIGESS_NOSTR_WALLET_CONNECT_RELAY
 const _nostrWalletConnectEncryptPrivKey = process.env.LIGESS_NOSTR_WALLET_CONNECT_PRIVATE_KEY
 const _nostrWalletConnectEncryptPubKey = _nostrWalletConnectEncryptPrivKey ? getPublicKey(_nostrWalletConnectEncryptPrivKey) : null
 
-const nostrConnectURL = `nostrwalletconnect://${_nostrWalletConnectEncryptPubKey}?relay=${encodeURI(_relay)}&secret=${_nostrWalletConnectEncryptPrivKey}`
+const nostrConnectURL = `nostr+walletconnect://${_nostrWalletConnectEncryptPubKey}?relay=${encodeURI(_relay)}&secret=${_nostrWalletConnectEncryptPrivKey}`
 
 console.log(nostrConnectURL)
 qrcode.generate(nostrConnectURL, {small: true})
