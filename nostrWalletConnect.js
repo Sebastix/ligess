300fdffafc55a16c8f7134cff3939c6741458fe2
mutatrum <mutatrum@gmail.com>
2024-09-05 10:56:42 +0200

    Add missing created_at to 23195 event

--- nostrWalletConnect.js
+++ nostrWalletConnect.js
@@ -222,7 +222,7 @@ async function processZapRequest(zapRequest, logger) {
   let response = {
     kind: 23195,
     pubkey: _nostrWalletConnectEncryptPubKey,
-    created_at: Date.now() / 1000,
+    created_at: Math.round(Date.now() / 1000),
     tags: [['e', zapRequest.id]]
   }
 
